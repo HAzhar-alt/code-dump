62b1466c0929b2cbf054d0cd9f6a46c1c0f8b5ff
HAzhar-alt <137207759+HAzhar-alt@users.noreply.github.com>
2025-05-22 11:34:29 +0100

    code along

---
+++ js/JavaScript Activities/CSS & JS/script.js
@@ -0,0 +1,15 @@
+function changeBocColour() {
+    let box1 = document.getElementById("box");
+
+    box1.style.backgroundColor - "blue"
+
+}
+
+changeBoxColour();
+
+function changeParaColour() {
+    let paragraphs = document
+getElementByTagName("p");
+    
+
+}
